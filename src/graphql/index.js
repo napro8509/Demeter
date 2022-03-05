7805f4a8dcaddbea8b182941cd7502ae384dcd1f
napro8509 <legiona14@gmail.com>
2022-03-05 17:02:05 +0700

    Add apollo clinent

---
+++ src/graphql/index.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import {
+	ApolloClient,
+	ApolloLink,
+	ApolloProvider,
+	HttpLink,
+	concat,
+	InMemoryCache,
+} from '@apollo/client';
+import { onError } from '@apollo/client/link/error';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { setContext } from '@apollo/client/link/context';
+
+import * as Constants from '../constants';
+
+const errorLink = onError(({ graphQLErrors, networkError }) => {
+	console.log('graphQLErrors', graphQLErrors);
+	console.log('networkError', networkError);
+	if (graphQLErrors)
+		graphQLErrors.forEach(({ message, locations, path }) =>
+			console.log(
+				`[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`
+			)
+		);
+	if (networkError) console.log(`[Network error]: ${networkError}`);
+});
+
+const getToken = async () => {
+	const token = await AsyncStorage.getItem(Constants.ASYNC_AUTH_TOKEN);
+	return JSON.parse(token);
+};
+
+const auth = setContext(async (operation, forward) => {
+	const token = await getToken();
+	const headers = {
+		Authorization: `Bearer ${token}`,
+	};
+
+	return { headers };
+});
+
+const httpLink = new HttpLink({
+	uri: Constants.GRAPHQL_URL,
+});
+
+const client = new ApolloClient({
+	link: concat(errorLink, concat(auth, httpLink)),
+	// link: concat(errorLink, httpLink),
+	cache: new InMemoryCache(),
+});
+
+const withApollo = WrappedComponent => {
+	return (
+		<ApolloProvider client={client}>
+			<WrappedComponent />
+		</ApolloProvider>
+	);
+};
+
+export default withApollo;
